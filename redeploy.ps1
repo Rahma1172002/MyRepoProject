ed516b200190e774ea6b3acb27a9e23f9cb6e36f
rahma <trabelsirahma541@gmail.com>
2026-05-15 21:52:20 +0100

    dockerfile

---
+++ redeploy.ps1
@@ -0,0 +1,17 @@
+# Variables
+$imageName = "rahma1234/mon-image:latest"
+$deploymentName = "mon-appli"
+
+Write-Host "Construction de l'image Docker..."
+docker build -t $imageName .
+
+Write-Host "Push vers Docker Hub..."
+docker push $imageName
+
+Write-Host "Redémarrage du déploiement Kubernetes..."
+kubectl rollout restart deployment $deploymentName
+
+Start-Sleep -Seconds 5
+
+Write-Host "Pods en cours..."
+kubectl get pods
